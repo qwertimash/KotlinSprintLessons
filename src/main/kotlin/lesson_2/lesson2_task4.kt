2215565bfd584435b1e02ce0c43c2c9cc06e3979
qwertimash <kostyatimashov6035@gmail.com>
2025-11-30 22:43:59 +0300

    KS-2-4 resolved, deleted excess file

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -3,10 +3,11 @@ package org.example.lesson_2
 fun main() {
     val crystalOre = 7
     val ironOre = 11
+    val bonusPercent = 20
 
+    val bonusCrystalOre = crystalOre * (bonusPercent / 100.0)
+    val bonusIronOre = ironOre * (bonusPercent / 100.0)
     println("Количество бонусных материалов:")
-    print("Кристаллической руды: ")
-    println(crystalOre / 5) // деление на 5 <=> умножение на 0.2, но делим целое на целое = целое
-    print("Железной руды: ")
-    println(ironOre / 5)
+    println("Кристаллической руды: %d".format(bonusCrystalOre.toInt()))
+    println("Железной руды: %d".format(bonusIronOre.toInt()))
 }

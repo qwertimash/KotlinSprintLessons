eaa864186b0a26b8bd39e688ece2ca3dc2709790
qwertimash <kostyatimashov6035@gmail.com>
2025-11-30 17:00:21 +0300

    KS-2-4 bonus ores calculated

---
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_2
+
+fun main() {
+
+    val crystallOre = 7
+    val ironOre = 11
+
+    println("Количество бонусных материалов:")
+    print("Кристаллической руды: ")
+    println(crystallOre / 5) // деление на 5 <=> умножение на 0.2, но делим целое на целое = целое
+    print("Железной руды: ")
+    println(ironOre / 5)
+
+}

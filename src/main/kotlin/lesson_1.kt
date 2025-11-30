4de49abed5739da07492d5eaafb877fb71b319b0
qwertimash <kostyatimashov6035@gmail.com>
2025-11-30 15:21:12 +0300

    KS-2 arithmetics study

---
+++ src/main/kotlin/lesson_1.kt
@@ -0,0 +1,19 @@
+package org.example
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+
+    var numberOfOrders: Int = 42
+    val welcomeMessage: String = "Welcome to our app!"
+
+//    println(numberOfOrders)
+    println(welcomeMessage)
+
+    numberOfOrders = 52
+
+    println(numberOfOrders)
+
+
+
+}

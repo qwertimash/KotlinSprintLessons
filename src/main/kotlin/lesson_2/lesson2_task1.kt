b0e38e41051fdb139d024496c5348265985f65e8
qwertimash <kostyatimashov6035@gmail.com>
2025-11-30 17:11:27 +0300

    KS-2-1 corrected types, formatting

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -1,16 +1,13 @@
 package org.example.lesson_2
 
 fun main() {
-
     val countOfStudents = 4
     val mark1 = 3.0
-    val mark2 = 4
-    val mark3 = 3
-    val mark4 = 5
+    val mark2 = 4.0
+    val mark3 = 3.0
+    val mark4 = 5.0
 
-    val averageMark = (mark1 + mark2 + mark3 + mark4) / countOfStudents
+    val averageMark = String.format("%.2f",(mark1 + mark2 + mark3 + mark4) / countOfStudents)
 
     println(averageMark)
-
-
 }

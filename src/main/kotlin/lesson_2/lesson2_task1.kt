3bb7a6afc370c9993df889ee4cd8b5790a34c349
qwertimash <kostyatimashov6035@gmail.com>
2025-11-30 15:41:22 +0300

    KS-2-1 average counted

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_2
+
+fun main() {
+
+    val countOfStudents = 4
+    val mark1 = 3.0
+    val mark2 = 4
+    val mark3 = 3
+    val mark4 = 5
+
+    val averageMark = (mark1 + mark2 + mark3 + mark4) / countOfStudents
+
+    println(averageMark)
+
+
+}

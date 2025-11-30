12435e2044888bcfe6674fe9db82fd12adac16fe
qwertimash <kostyatimashov6035@gmail.com>
2025-11-30 17:16:39 +0300

    KS-2-1 correct logic

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -7,7 +7,7 @@ fun main() {
     val mark3 = 3.0
     val mark4 = 5.0
 
-    val averageMark = String.format("%.2f",(mark1 + mark2 + mark3 + mark4) / countOfStudents)
+    val averageMark =(mark1 + mark2 + mark3 + mark4) / countOfStudents
 
-    println(averageMark)
+    println(String.format("%.2f", averageMark))
 }

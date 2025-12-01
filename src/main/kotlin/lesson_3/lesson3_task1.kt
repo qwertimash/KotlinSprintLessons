ce2ef5e9d1434d53a771ee1b87fa1b3c5b95043e
qwertimash <kostyatimashov6035@gmail.com>
2025-12-01 17:05:42 +0300

    KS-3-1 diff time greet

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,9 @@
+package lesson_3
+
+fun main() {
+    val userName = "Константин"
+    var greeting = "Доброе утро, $userName!"
+    println(greeting)
+    greeting = "Добрый вечер, $userName!"
+    println(greeting)
+}

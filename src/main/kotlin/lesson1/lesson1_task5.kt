16017541730b7581057018d9c77f326f9569ba06
qwertimash <kostyatimashov6035@gmail.com>
2025-12-01 16:25:04 +0300

    KS-1-5 const value, nice print

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -1,16 +1,13 @@
 package org.example.lesson1
 
+const val UNITS_OF_TIME = 60
+
 fun main() {
-
     val seconds = 6480
-    val minutes = seconds / 60
-    val remainderOfSeconds = seconds % 60
-    val hours = minutes / 60
-    val remainderOfMinutes = minutes % 60
+    val minutes = seconds / UNITS_OF_TIME
+    val remainderOfSeconds = seconds % UNITS_OF_TIME
+    val hours = minutes / UNITS_OF_TIME
+    val remainderOfMinutes = minutes % UNITS_OF_TIME
 
-    print(String.format("%02d",hours))
-    print(':')
-    print(String.format("%02d",remainderOfMinutes))
-    print(':')
-    print(String.format("%02d",remainderOfSeconds))
+    print("%02d:%02d:%02d".format(hours,remainderOfMinutes,remainderOfSeconds))
 }

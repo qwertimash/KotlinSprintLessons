9e716e843ed1ff58f2b1f1b0e63c42b1b007fbd6
qwertimash <kostyatimashov6035@gmail.com>
2025-12-01 17:13:25 +0300

    KS-3-2 before, after marriage user info

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,11 @@
+package lesson_3
+
+fun main() {
+    var userAge = 20
+    var userFullName = "Андреева Татьяна Сергеевна"
+    println("$userFullName, $userAge")
+
+    userAge = 22
+    userFullName = "Сидорова Татьяна Сергеевна"
+    println("$userFullName, $userAge")
+}

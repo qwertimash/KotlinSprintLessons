daea5b07603d1abf76a4837e49e7a723731b7c8a
qwertimash <kostyatimashov6035@gmail.com>
2025-12-01 21:52:05 +0300

    KS-2-5 const value added, cmd+opt+l pressed

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -2,12 +2,14 @@ package org.example.lesson_2
 
 import kotlin.math.pow
 
+const val FULL_PERCENT = 100
+
 fun main() {
     val startValue = 70_000.0
     val annualRate = 16.7
     val years = 20.0
 
-    val totalSum = startValue * (1 + annualRate/100).pow(years)
+    val totalSum = startValue * (1 + annualRate / FULL_PERCENT).pow(years)
 
     print("%.3f".format(totalSum))
 }

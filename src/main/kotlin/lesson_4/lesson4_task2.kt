2682fb5a79e38cd4308066743a4f00cc8d07f03c
qwertimash <kostyatimashov6035@gmail.com>
2025-12-02 23:47:04 +0300

    KS-4-2 cargo compliance

---
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -0,0 +1,15 @@
+package lesson_4
+
+const val AVERAGE_MIN_WEIGHT = 35
+const val AVERAGE_MAX_WEIGHT = 100
+const val AVERAGE_MAX_VOLUME = 100
+
+fun main() {
+    val firstCargoWeight = 20
+    val firstCargoVolume = 80
+    val secondCargoWeight = 50
+    val secondCargoVolume = 100
+
+    println("Груз с весом $firstCargoWeight кг и объемом $firstCargoVolume л соответствует категории 'Average': ${((AVERAGE_MIN_WEIGHT < firstCargoWeight) && (firstCargoWeight <= AVERAGE_MAX_WEIGHT)) && (firstCargoVolume < AVERAGE_MAX_VOLUME)}")
+    println("Груз с весом $secondCargoWeight кг и объемом $secondCargoVolume л соответствует категории 'Average': ${((AVERAGE_MIN_WEIGHT < secondCargoWeight) && (secondCargoWeight <= AVERAGE_MAX_WEIGHT)) && (secondCargoVolume < AVERAGE_MAX_VOLUME)}")
+}

093b4ea7f7a285c7991019cd4e8a7a704200db5e
qwertimash <kostyatimashov6035@gmail.com>
2025-12-03 00:02:34 +0300

    KS-4-3 checked requirements

---
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -0,0 +1,15 @@
+package lesson_4
+
+const val REQUIRED_WEATHER_IS_SUNNY = true
+const val REQUIRED_TENT_IS_OPEN = true
+const val REQUIRED_AIR_HUMIDITY_PERCENT = 20
+const val EXCEPTIONAL_SEASON = "winter"
+
+fun main() {
+    val weatherIsSunny = true
+    val tentIsOpen = true
+    val airHumidityPercent = 20
+    val currentSeason = "winter"
+
+    println("Благоприятные ли условия сейчас для роста бобовых? ${(weatherIsSunny == REQUIRED_WEATHER_IS_SUNNY) && (tentIsOpen == REQUIRED_TENT_IS_OPEN) && (airHumidityPercent == REQUIRED_AIR_HUMIDITY_PERCENT) && (currentSeason != EXCEPTIONAL_SEASON)}")
+}

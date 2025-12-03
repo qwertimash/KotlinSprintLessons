20914d9beadfc47de1c29c2e0da8e31cc8ac6df9
qwertimash <kostyatimashov6035@gmail.com>
2025-12-03 15:50:08 +0300

    KS-4-3 variables renamed, lines splited

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -6,10 +6,17 @@ const val REQUIRED_AIR_HUMIDITY_PERCENT = 20
 const val EXCEPTIONAL_SEASON = "winter"
 
 fun main() {
-    val weatherIsSunny = true
-    val tentIsOpen = true
+    val isSunny = true
+    val isAwningOpen = true
     val airHumidityPercent = 20
     val currentSeason = "winter"
 
-    println("Благоприятные ли условия сейчас для роста бобовых? ${(weatherIsSunny == REQUIRED_WEATHER_IS_SUNNY) && (tentIsOpen == REQUIRED_TENT_IS_OPEN) && (airHumidityPercent == REQUIRED_AIR_HUMIDITY_PERCENT) && (currentSeason != EXCEPTIONAL_SEASON)}")
+    println(
+        "Благоприятные ли условия сейчас для роста бобовых? ${
+            (isSunny == REQUIRED_WEATHER_IS_SUNNY) &&
+                    (isAwningOpen == REQUIRED_TENT_IS_OPEN) &&
+                    (airHumidityPercent == REQUIRED_AIR_HUMIDITY_PERCENT) &&
+                    (currentSeason != EXCEPTIONAL_SEASON)
+        }"
+    )
 }

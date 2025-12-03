411de379c3d703f5000f55acc0d80ca21b2e85ff
qwertimash <kostyatimashov6035@gmail.com>
2025-12-03 15:45:59 +0300

    KS-4-2 lines shorted

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -10,6 +10,17 @@ fun main() {
     val secondCargoWeight = 50
     val secondCargoVolume = 100
 
-    println("Груз с весом $firstCargoWeight кг и объемом $firstCargoVolume л соответствует категории 'Average': ${((AVERAGE_MIN_WEIGHT < firstCargoWeight) && (firstCargoWeight <= AVERAGE_MAX_WEIGHT)) && (firstCargoVolume < AVERAGE_MAX_VOLUME)}")
-    println("Груз с весом $secondCargoWeight кг и объемом $secondCargoVolume л соответствует категории 'Average': ${((AVERAGE_MIN_WEIGHT < secondCargoWeight) && (secondCargoWeight <= AVERAGE_MAX_WEIGHT)) && (secondCargoVolume < AVERAGE_MAX_VOLUME)}")
+    val firstCargoCompliance = ((AVERAGE_MIN_WEIGHT < firstCargoWeight) && (firstCargoWeight <= AVERAGE_MAX_WEIGHT))
+            && (firstCargoVolume < AVERAGE_MAX_VOLUME)
+    val secondCargoCompliance = ((AVERAGE_MIN_WEIGHT < secondCargoWeight) && (secondCargoWeight <= AVERAGE_MAX_WEIGHT))
+            && (secondCargoVolume < AVERAGE_MAX_VOLUME)
+
+    println(
+        "Груз с весом $firstCargoWeight кг и объемом $firstCargoVolume л " +
+                "соответствует категории 'Average': ${firstCargoCompliance}"
+    )
+    println(
+        "Груз с весом $secondCargoWeight кг и объемом $secondCargoVolume л " +
+                "соответствует категории 'Average': ${secondCargoCompliance}"
+    )
 }

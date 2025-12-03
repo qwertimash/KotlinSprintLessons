91352a3b8930397d5c9861f58fad69336714f88f
qwertimash <kostyatimashov6035@gmail.com>
2025-12-03 15:56:44 +0300

    KS-4-4 new variable of parity of current day

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -4,15 +4,16 @@ const val NAME_DAY_OF_ABS_AND_HANDS = "a&h"
 const val NAME_DAY_OF_BACK_AND_LEGS = "b&l"
 
 fun main() {
-    val currentDay = 3
+    val currentDay = 5
     val firstDayExercises = NAME_DAY_OF_ABS_AND_HANDS
+    val parityOfCurrentDay = currentDay % 2 == 0
 
     println(
         """
-        Упражнения для рук:    ${((currentDay % 2 == 1) && (firstDayExercises == NAME_DAY_OF_ABS_AND_HANDS)) || ((currentDay % 2 == 0) && (firstDayExercises == NAME_DAY_OF_BACK_AND_LEGS))}
-        Упражнения для ног:    ${((currentDay % 2 == 1) && (firstDayExercises != NAME_DAY_OF_ABS_AND_HANDS)) || ((currentDay % 2 == 0) && (firstDayExercises != NAME_DAY_OF_BACK_AND_LEGS))}
-        Упражнения для спины:  ${((currentDay % 2 == 1) && (firstDayExercises != NAME_DAY_OF_ABS_AND_HANDS)) || ((currentDay % 2 == 0) && (firstDayExercises != NAME_DAY_OF_BACK_AND_LEGS))}
-        Упражнения для пресса: ${((currentDay % 2 == 1) && (firstDayExercises == NAME_DAY_OF_ABS_AND_HANDS)) || ((currentDay % 2 == 0) && (firstDayExercises == NAME_DAY_OF_BACK_AND_LEGS))}
+        Упражнения для рук:    ${(!parityOfCurrentDay && (firstDayExercises == NAME_DAY_OF_ABS_AND_HANDS)) || (parityOfCurrentDay && (firstDayExercises == NAME_DAY_OF_BACK_AND_LEGS))}
+        Упражнения для ног:    ${(!parityOfCurrentDay && (firstDayExercises != NAME_DAY_OF_ABS_AND_HANDS)) || (parityOfCurrentDay && (firstDayExercises != NAME_DAY_OF_BACK_AND_LEGS))}
+        Упражнения для спины:  ${(!parityOfCurrentDay && (firstDayExercises != NAME_DAY_OF_ABS_AND_HANDS)) || (parityOfCurrentDay && (firstDayExercises != NAME_DAY_OF_BACK_AND_LEGS))}
+        Упражнения для пресса: ${(!parityOfCurrentDay && (firstDayExercises == NAME_DAY_OF_ABS_AND_HANDS)) || (parityOfCurrentDay && (firstDayExercises == NAME_DAY_OF_BACK_AND_LEGS))}
     """.trimIndent()
     )
 }
